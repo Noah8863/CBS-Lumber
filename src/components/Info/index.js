0fb37166667d11b58ca5962002a1005233b164b5
Noah8863 <stacknoah99@gmail.com>
2022-10-22 16:06:17 -0600

    finished another info card for main page

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -3,30 +3,57 @@ import "./info.scss";
 
 function Info() {
   return (
-    <div className="main-info-container">
-      <div className="info-containers">
-        <div className="info-cards">
-          <div>
-            <h1 className="card-header">WHO WE ARE</h1>
-          </div>
-          <div>
-            <p>
-              Established in 2020 CBS-Lumber is a wholesale bulk lumber supply.
-              Our lumber supply includes OSB, OSB Tongue and Groove, Plywood,
-              and traditional lumber, plus a few more products coming soon. For
-              all your building needs in the greater Denver, Golden,
-              Westminster, Thornton, Broomfield, Boulder, Lakewood, Littleton,
-              Highlands Ranch, Centennial, Parker, Aurora, Brighton, and other
-              surrounding areas. We are a team of fully certified professionals
-              who tackle everything from complex large projects to smaller scale
-              commercial jobs. Fueled by our commitment to excellence, Colorado
-              Building Solutions goes the extra mile to make sure clients are
-              completely satisfied with our work.
-            </p>
+    <>
+      <div className="main-info-container">
+        <div className="info-containers">
+          <div className="info-cards">
+            <div>
+              <h1 className="card-header">WHO WE ARE</h1>
+            </div>
+            <div>
+              <p>
+                Established in 2020 CBS-Lumber is a wholesale bulk lumber
+                supply. Our lumber supply includes OSB, OSB Tongue and Groove,
+                Plywood, and traditional lumber, plus a few more products coming
+                soon. For all your building needs in the greater Denver, Golden,
+                Westminster, Thornton, Broomfield, Boulder, Lakewood, Littleton,
+                Highlands Ranch, Centennial, Parker, Aurora, Brighton, and other
+                surrounding areas. We are a team of fully certified
+                professionals who tackle everything from complex large projects
+                to smaller scale commercial jobs. Fueled by our commitment to
+                excellence, Colorado Building Solutions goes the extra mile to
+                make sure clients are completely satisfied with our work.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="main-info-container">
+        <div className="info-containers">
+          <div className="info-cards">
+            <div>
+              <h1 className="card-header">ABOUT US</h1>
+            </div>
+            <div>
+              <p>
+                CBS-Lumber supply division, based out of Englewood, COlorado,
+                was founded back in 2020 when our owners saw the impact the
+                supply shortages brought to us. Our goal is to bring the most
+                affordable, quality lumber products for either pick up or
+                delivery. We value the relationships we form with our customers
+                and strive to supply you with what you need. CBS-Lumber is proud
+                to server Denver, GOlder, Westminster, Thornton, Broomfield,
+                Boulder, Lakewood, Littleton, Highlands Ranch, Centennial,
+                Parker, Aurora, Brighton, and other surrounding areas. Colorado
+                Building Solutions has partnerships with Nova Incorporations. We
+                take pride in all of our services we offer and strive to provide
+                you with great service.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
