b6c2544f474170666cf34d41169afad92e0a022f
Noah8863 <stacknoah99@gmail.com>
2022-10-25 12:38:52 -0600

    deleted email and started product page

--- src/components/Contact-Card/index.js
+++ src/components/Contact-Card/index.js
@@ -7,7 +7,6 @@ function ContactCard(){
             <div id="contact-card">
                 <h1>CONTACT US</h1>
                 <p>www.CBS-Lumber.com</p>
-                <a href="mailto:Cobuildingsolutions@gmail.com"><p>Cobuildingsolutions@gmail.com</p></a>
                 <a href="mailto:info@cbs-lumber.com"><p>info@CBS-Lumber.com</p></a>
                 <a href="tel:7207561299"><p>(720) 756-1299</p></a>
             </div>
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import './product.scss';
-
+import React from "react";
+import "./product.scss";
+import leftPicture from "../../images/product-wood-img.jpg";
+import rightPicture from "../../images/product-wood-img-1.jpg";
+import Certification from "../Certification/index";
+import Contact from "../Contact-Card/index";
 function Products() {
-    return (
-        <div id="project-header-container">
-            
-            
-                <div id="header-left"></div>
-                <div id="header-right"></div>
-            
+  return (
+    <>
+      <h1 id="header-description">AFFORDABLE LUMBER FOR ANY PROJECT</h1>
+      <div id="product-header-container">
+        <div id="header-left">
+          <img src={leftPicture}></img>
         </div>
-    )
+        <div id="header-right">
+          <img src={rightPicture}></img>
+        </div>
+      </div>
+      <div id="lumber-description-container">
+        <div id="lumber-description-card">
+          <h1>LUMBER</h1>
+          <p>
+            Colorado Building Solutions now has a bulk lumber supply at our
+            warehouse, with supply avaliable today for pickup or delivery. CBS
+            Lumber provides a variety of lumber to meet all of your building
+            needs. Our Lumber supply includes OSB, OSB Tongue and Groove,
+             Plywood, and traditional lumber. With a few other products being
+            added soon. CBS Lumber is a wholesale lumber wendor serving Denver,
+            Golden, Westminster, Thornton, Broomfield, Boulder, Lakewood,
+            Littleton, Highlands Ranch, Centennial, Parker, Aurora, Brighton,
+            and other surrounding areas.
+          </p>
+        </div>
+      </div>
+      <Certification />
+      <Contact />
+    </>
+  );
 }
 
 export default Products;
